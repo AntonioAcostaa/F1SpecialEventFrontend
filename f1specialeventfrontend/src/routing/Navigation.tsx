bdbd01aa5670a0713cd12baecfb647464b1a1502
antonioacosta <antonio.acosta@intility.no>
2023-11-08 21:12:12 +0100

    Modified the navbar to include the logo and som e tweeks to keep them in line

--- f1specialeventfrontend/src/routing/Navigation.tsx
+++ f1specialeventfrontend/src/routing/Navigation.tsx
@@ -4,11 +4,12 @@ import { Link } from "react-router-dom"
 const Navigation = () => {      
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container">
-            <a className="navbar-brand" href="#">F1 Special Event</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+            <div className="container d-flex slign-items-center" >
+                <img className="navbar-brand" src="/src/assets/images/f1-logo.png" alt="page-header-image. PNG" style={{width: "80px", margin: "2px 0 0 0"}}></img>
+                <p className="navbar-brand" style={{margin: "0 10px 0 0", padding: 0, fontStyle: "italic"}}>Special Event</p>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
