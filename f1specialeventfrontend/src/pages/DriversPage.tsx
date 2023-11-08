542b68930081ea8e355adabd9eb66c8135669bc5
antonioacosta <antonio.acosta@intility.no>
2023-11-08 15:32:03 +0100

    Testing getAllDrivers on driverpage

--- f1specialeventfrontend/src/pages/DriversPage.tsx
+++ f1specialeventfrontend/src/pages/DriversPage.tsx
@@ -1,8 +1,36 @@
-
+import { useState } from "react";
+import IDriver from "../interfaces/IDriver";
+import DriversService from "../services/DriversService";
 
 const DriversPage = () => {
+
+    const [drivers, setDrivers] = useState<IDriver[]>([]);
+
+    const getAllDrivers = () => {
+        DriversService.getAllDrivers()
+            .then(response => {
+                setDrivers(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
     return (
+        <div className="container">
         <div>Drivers</div>
+        <button onClick={getAllDrivers}>Get all drivers</button>
+
+        <div>
+            {drivers && drivers.map((driver) => {
+                return (
+                    <div key={driver.id}>
+                        {driver.id} {driver.name} {driver.age} {driver.nationality}
+                    </div>
+                );
+            })}
+        </div>
+        </div>
     );
 }
 
