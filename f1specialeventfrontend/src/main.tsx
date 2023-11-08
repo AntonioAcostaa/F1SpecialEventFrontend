a97ab0220a98e213023703c9646527d930c13c1d
antonioacosta <antonio.acosta@intility.no>
2023-11-08 15:26:12 +0100

    Videre forbedringer

--- f1specialeventfrontend/src/main.tsx
+++ f1specialeventfrontend/src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import 'bootstrap/dist/css/bootstrap.css';   // Importing bootstrap css
+import 'bootstrap/dist/css/bootstrap.css';   // Importerer bootstrap css
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Importerer bootstrap js
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
--- f1specialeventfrontend/src/pages/HomePage.tsx
+++ f1specialeventfrontend/src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Home bye</h1>
         </div>
     );
 }
