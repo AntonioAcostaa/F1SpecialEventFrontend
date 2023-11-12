7070e47bd21af516e351744ad267f75f184663ab
Ottoro <alexander.robsahm@gmail.com>
2023-11-12 19:21:18 +0100

    fikset kort

--- f1specialeventfrontend/src/pages/drivers/components/DriverCard.tsx
+++ f1specialeventfrontend/src/pages/drivers/components/DriverCard.tsx
@@ -4,21 +4,26 @@ import styles from "../styles/driverList.module.css";
 const DriverCard = ({ driver }: { driver: IDriver }) => {
     return (
         <article
-            className={`${styles.cardContainer} border-3 rounded shadow border-bottom border-danger mx-auto text-center p-3 m-1`}
-            style={{ width: "200px", padding: 0 }}
+            className={`${styles.cardContainer} border-3 rounded shadow border-bottom border-danger mx-auto text-center p-1 m-4`}
+            style={{ width: "300px", padding: 0 }}
         >
             <img
                 src={`http://localhost:5014/images/${driver.image}`}
-                className="card-img-top"
+                className="card-img-top rounded"
                 alt={`Portret image. PNG. ${driver.name}`}
             />
-            <div className="card-body">
-                <h5 className="card-title">{driver.name}</h5>
-                <p className="card-text">
-                    Age: {driver.age}
-                    <br></br>
-                    From: {driver.nationality}
-                </p>
+            <div className="card-body ">
+                <h5 className="card-title p-1">{driver.name}</h5>
+                <div className="card-text p-2 text-start">
+                    <div className="d-flex gap-2">
+                        <p style={{ margin: 0, fontWeight: "700" }}>Age:</p>{" "}
+                        {driver.age}
+                    </div>
+                    <div className="d-flex gap-2">
+                        <p style={{ margin: 0, fontWeight: "700" }}>From:</p>{" "}
+                        {driver.nationality}
+                    </div>
+                </div>
             </div>
             <div className="card-footer">
                 <small className="text-muted">Driver ID: {driver.id}</small>
--- f1specialeventfrontend/src/pages/drivers/components/DriverList.tsx
+++ f1specialeventfrontend/src/pages/drivers/components/DriverList.tsx
@@ -1,16 +1,15 @@
 import IDriver from "../../../interfaces/IDriver";
 import DriverCard from "./DriverCard";
 
-const DriverList = ({drivers} : {drivers: IDriver[]}) => {
+const DriverList = ({ drivers }: { drivers: IDriver[] }) => {
     return (
-        <section className="row gap-1">
-            { drivers && drivers.map((driver) => {
-                return(
-                    <DriverCard key={driver.id} driver={driver} />
-                )
-            })}
+        <section className="row gap-5">
+            {drivers &&
+                drivers.map((driver) => {
+                    return <DriverCard key={driver.id} driver={driver} />;
+                })}
         </section>
-    )
+    );
 };
 
 export default DriverList;
