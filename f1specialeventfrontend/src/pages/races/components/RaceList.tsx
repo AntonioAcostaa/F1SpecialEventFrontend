27479ae67d3834b6a741879fec720b36b15b9b8f
Ottoro <alexander.robsahm@gmail.com>
2023-11-12 14:38:51 +0100

    card race

--- f1specialeventfrontend/src/pages/races/components/RaceList.tsx
+++ f1specialeventfrontend/src/pages/races/components/RaceList.tsx
@@ -9,36 +9,42 @@ interface RaceListProps {
 
 const RaceList: React.FC<RaceListProps> = ({ races }) => {
     return (
-        <div className="container">
-            <div className="test">
-                <div className="row race-list-heade m-2 border-2">
-                    <div className="col m-2">ID</div>
-                    <div className="col m-2">Grand Prix</div>
-                    <div className="col m-2">Winner</div>
-                    <div className="col m-2">Winner Time</div>
-                    <div className="col m-2">Number of Laps</div>
-                    <div className="col m-2">Track</div>
-                </div>
-                {races.map((race) => (
-                    <div
-                        key={race.id}
-                        className="row race-list-item m-2 border-2 border shadow rounded "
-                    >
-                        <div className="col m-3">{race.id}</div>
-                        <div className="col m-3">{race.grandPrix}</div>
-                        <div className="col m-3">{race.winnerName}</div>
-                        <div className="col m-3">
-                            {format(new Date(race.winnerTime), "HH:mm:ss")}
+        <div className="container ">
+            {races.map((race) => (
+                <div className="col-12 mx-auto text-center p-3 border-start shadow border-bottom border-2 rounded border-danger m-5">
+                    <div className="row g-0">
+                        <div className="col-md-4">
+                            <img
+                                src={`http://localhost:5014/images/${race.image}`}
+                                className="rounded w-100 h-100 shadow"
+                                alt={`Track image. PNG. ${race.grandPrix}`}
+                            />
                         </div>
-                        <div className="col m-3">{race.numberOfLaps}</div>
-                        <img
-                            src={`http://localhost:5014/images/${race.image}`}
-                            className="card mb-3"
-                            alt={`Track image. PNG. ${race.grandPrix}`}
-                        />
+                        <div className="col-md-8">
+                            <div className="card-body">
+                                <h3 className="card-title">{race.grandPrix}</h3>
+                                <br />
+                                <br />
+                                <h5 className="card-text">Winner:</h5>
+                                <p className="card-text">{race.winnerName}</p>
+                                <h5 className="card-text">Time:</h5>
+                                <p className="card-text">
+                                    {format(
+                                        new Date(race.winnerTime),
+                                        "HH:mm:ss"
+                                    )}
+                                </p>
+                                <br />
+                                <p className="card-text">
+                                    <small className="id-text">
+                                        ID: {race.id}
+                                    </small>
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                ))}
-            </div>
+                </div>
+            ))}
         </div>
     );
 };
