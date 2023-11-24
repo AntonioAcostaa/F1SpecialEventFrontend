a3894e7cafc01608e5dce0e63920be5648420a17
antonioacosta <antonio.acosta@intility.no>
2023-11-24 22:46:55 +0100

    Changed footer from fixed to static

--- f1specialeventfrontend/src/components/shared/Footer.tsx
+++ f1specialeventfrontend/src/components/shared/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className='footer border-top border-danger border-2 bg-dark text-white' style={{position: "fixed", bottom: "0", width: "100%", height: "40px", textAlign: "center", display: "flex", alignItems: "center", justifyContent: "center"}}>
+        <footer className='footer border-top border-danger border-2 bg-dark text-white' style={{position: "static", bottom: "0", width: "100%", height: "40px", textAlign: "center", display: "flex", alignItems: "center", justifyContent: "center"}}>
             <div>
                 <span>&copy; 2023. Laget av to studenter. Powered by React TSX, .NET</span>
             </div>
