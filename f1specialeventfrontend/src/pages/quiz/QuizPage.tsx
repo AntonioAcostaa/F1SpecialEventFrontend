b36adb868c4d05b36797f4a6d795d1d3f621a4f7
Ottoro <alexander.robsahm@gmail.com>
2023-11-26 11:08:52 +0100

    det jeg glemte

--- f1specialeventfrontend/src/pages/quiz/QuizPage.tsx
+++ f1specialeventfrontend/src/pages/quiz/QuizPage.tsx
@@ -26,7 +26,6 @@ const QuizPage = () => {
     const [score, setScore] = useState(0);
     const [isQuizStarted, setIsQuizStarted] = useState(false);
     const [name, setName] = useState("");
-    //const scoreArray: { name: string; score: number }[] = [];
 
     const [selectedAnswer, setSelectedAnswer] = useState<{
         id: number;
@@ -55,48 +54,60 @@ const QuizPage = () => {
 
             return (
                 <>
-                <div className="container m-5">
-                    <h2>You have reached the end of the quiz!</h2>
-                    <div className="cardHeader m-5">
-                        Your score is:{" "}
-                        <h2 className="text-danger p-2">{score}</h2>
-                    </div>
-                    <h2 className="m-5">Scores:</h2>
-                    <div className="scores shadow rounded border-bottom border-5 border-danger border-start">
-                        <div className="scoresFont d-flex flex-column ">
-                            {scoreArray.map(
-                                (score: { name: string }, index: number) => (
-                                    <h5 key={index} className="scoresFont p-2">
-                                        {score.name}
-                                    </h5>
-                                )
-                            )}
+                    <div className="container ">
+                        <h2>You have reached the end of the quiz!</h2>
+                        <div className="cardHeader m-5">
+                            Your score is:{" "}
+                            <h2 className="text-danger p-2">{score}</h2>
                         </div>
+                        <h2 className="m-5">Scores:</h2>
+                        <div className="scores shadow rounded border-bottom border-5 border-danger border-start">
+                            <div className="scoresFont d-flex flex-column ">
+                                {scoreArray.map(
+                                    (
+                                        score: { name: string },
+                                        index: number
+                                    ) => (
+                                        <h5
+                                            key={index}
+                                            className="scoresFont p-2"
+                                        >
+                                            {score.name}
+                                        </h5>
+                                    )
+                                )}
+                            </div>
 
-                        <div className="scoresFont d-flex flex-column ">
-                            {scoreArray.map(
-                                (score: { score: number }, index: number) => (
-                                    <h5 key={index} className="text-danger p-2">
-                                        {score.score}
-                                    </h5>
-                                )
-                            )}
+                            <div className="scoresFont d-flex flex-column ">
+                                {scoreArray.map(
+                                    (
+                                        score: { score: number },
+                                        index: number
+                                    ) => (
+                                        <h5
+                                            key={index}
+                                            className="text-danger p-2"
+                                        >
+                                            {score.score}
+                                        </h5>
+                                    )
+                                )}
+                            </div>
+                        </div>
+                        <div className="m-3">
+                            <button
+                                className="btn btn-danger btn-primary"
+                                onClick={() => {
+                                    setIsQuizStarted(false);
+                                    setCurrentQuestionIndex(0);
+                                    setScore(0);
+                                }}
+                            >
+                                Restart the Quiz
+                            </button>
                         </div>
                     </div>
-                    <div className="m-3">
-                        <button
-                            className="btn btn-danger btn-primary"
-                            onClick={() => {
-                                setIsQuizStarted(false);
-                                setCurrentQuestionIndex(0);
-                                setScore(0);
-                            }}
-                        >
-                            Restart the Quiz
-                        </button>
-                    </div>
-                </div>
-                <Footer/>
+                    <Footer />
                 </>
             );
         }
@@ -104,59 +115,62 @@ const QuizPage = () => {
         const question = questions[currentQuestionIndex];
         return (
             <>
-            <div
-                className="container d-flex flex-column justify-content-between"
-                style={{ height: "100%" }}
-            >
-                <div>
-                    <h3 className="header border-5 rounded shadow border-bottom border-danger mx-auto text-center p-3 m-5">
-                        {question.question}
-                    </h3>
-                    {question.answers.map((answer) => (
-                        <div
-                            className="answer border-1 border-danger rounded shadow"
-                            style={{
-                                cursor: "pointer",
-                                border: "2px solid black",
-                                padding: "10px",
-                                margin: "10px",
+                <div
+                    className="container d-flex flex-column justify-content-between"
+                    style={{ height: "100%" }}
+                >
+                    <div>
+                        <h3 className="header border-5 rounded shadow border-bottom border-danger mx-auto text-center p-3 m-5">
+                            {question.question}
+                        </h3>
+                        {question.answers.map((answer) => (
+                            <div
+                                className="answer border-1 border-danger rounded shadow"
+                                style={{
+                                    cursor: "pointer",
+                                    border: "2px solid black",
+                                    padding: "10px",
+                                    margin: "10px",
+                                }}
+                                onClick={() => handleAnswerClick(answer)}
+                            >
+                                {answer.answer}
+                            </div>
+                        ))}
+                        {selectedAnswer && (
+                            <h4 className="cardHeader m-5">
+                                {selectedAnswer.isCorrect
+                                    ? "Correct!"
+                                    : "Wrong!"}
+                                <br />
+                            </h4>
+                        )}
+                    </div>
+                    <div>
+                        {score > 0 && (
+                            <div className="cardHeader m-3 ">
+                                Score:{" "}
+                                <h2 className="text-danger p-2">{score}</h2>
+                            </div>
+                        )}
+                    </div>
+                    <div className="d-flex justify-content-end mt-auto">
+                        <button
+                            className="btn btn-danger mx-1 mt-3"
+                            onClick={() => {
+                                if (currentQuestionIndex < questions.length) {
+                                    setCurrentQuestionIndex(
+                                        currentQuestionIndex + 1
+                                    );
+                                    setSelectedAnswer(null);
+                                }
                             }}
-                            onClick={() => handleAnswerClick(answer)}
                         >
-                            {answer.answer}
-                        </div>
-                    ))}
-                    {selectedAnswer && (
-                        <h4 className="cardHeader m-5">
-                            {selectedAnswer.isCorrect ? "Correct!" : "Wrong!"}
-                            <br />
-                        </h4>
-                    )}
+                            Next
+                        </button>
+                    </div>
                 </div>
-                <div>
-                    {score > 0 && (
-                        <div className="cardHeader m-3 ">
-                            Score: <h2 className="text-danger p-2">{score}</h2>
-                        </div>
-                    )}
-                </div>
-                <div className="d-flex justify-content-end mt-auto">
-                    <button
-                        className="btn btn-danger mx-1 mt-3"
-                        onClick={() => {
-                            if (currentQuestionIndex < questions.length) {
-                                setCurrentQuestionIndex(
-                                    currentQuestionIndex + 1
-                                );
-                                setSelectedAnswer(null);
-                            }
-                        }}
-                    >
-                        Next
-                    </button>
-                </div>
-            </div>
-            <Footer/>
+                <Footer />
             </>
         );
     };
@@ -175,58 +189,61 @@ const QuizPage = () => {
     if (!isQuizStarted) {
         return (
             <>
-            <div className="container">
-                <div className="header col-12 mx-auto rounded text-center p-4 border-top border-5 border-danger border-end mb-1 mt-3">
-                    <h3 className="">Enter your name to start the quiz</h3>
-                    <br />
-                    <form
-                        className="d-flex flex-column justify-content-center align-items-center"
-                        onSubmit={(e) => {
-                            e.preventDefault();
-                            if (name !== "") {
-                                setIsQuizStarted(true);
-                            }
-                        }}
-                    >
-                        <input
-                            type="text"
-                            placeholder="Your name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                        />
-                        <button type="submit" className="btn btn-danger m-2">
-                            Start Quiz
-                        </button>
-                    </form>
+                <div className="container">
+                    <div className="header col-12 mx-auto rounded text-center p-4 border-top border-5 border-danger border-end mb-1 mt-3">
+                        <h3 className="">Enter your name to start the quiz</h3>
+                        <br />
+                        <form
+                            className="d-flex flex-column justify-content-center align-items-center"
+                            onSubmit={(e) => {
+                                e.preventDefault();
+                                if (name !== "") {
+                                    setIsQuizStarted(true);
+                                }
+                            }}
+                        >
+                            <input
+                                type="text"
+                                placeholder="Your name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                            <button
+                                type="submit"
+                                className="btn btn-danger m-2"
+                            >
+                                Start Quiz
+                            </button>
+                        </form>
+                    </div>
                 </div>
-            </div>
-            <Footer/>
+                <Footer />
             </>
         );
     }
 
     return (
         <>
-        <div className="container">
-            <div>
-                <div className="header col-12 mx-auto rounded text-center p-4 border-top border-5 border-danger border-end mb-1 mt-3">
-                    <h1>How well do you know the F1 teams?</h1>
-                </div>
+            <div className="container">
+                <div>
+                    <div className="header col-12 mx-auto rounded text-center p-4 border-top border-5 border-danger border-end mb-1 mt-3">
+                        <h1>How well do you know the F1 teams?</h1>
+                    </div>
 
-                <div
-                    className={` rounded shadow mx-auto text-center p-3  m-5`}
-                    style={{
-                        display: "flex",
-                        width: "100%",
-                        height: "800px",
-                        border: "1px solid red",
-                    }}
-                >
-                    {listQuiz()}
+                    <div
+                        className={` rounded shadow mx-auto text-center p-3  m-5`}
+                        style={{
+                            display: "flex",
+                            width: "100%",
+                            height: "800px",
+                            border: "1px solid red",
+                        }}
+                    >
+                        {listQuiz()}
+                    </div>
                 </div>
             </div>
-        </div>
-        <Footer/>
+            <Footer />
         </>
     );
 };
